4c18c23b50930ce565af2ef4ff0bfe032439fe99
Asraful Samad Chowdhury <asraful.chowdhury@pathao.com>
2022-12-22 15:49:12 +0600

    - Adding solution of LC-242(Valid Anagram)

---
+++ src/main/java/com/sindhuk/leetcode/ds1/LC_242.java
@@ -0,0 +1,30 @@
+package main.java.com.sindhuk.leetcode.ds1;
+
+public class LC_242 {
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int count[] = new int[26];
+        for(int i=0;i<s.length();i++) {
+            count[s.charAt(i) - 97] += 1;
+            count[t.charAt(i) - 97] -= 1;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] arg){
+        LC_242 lc_242 = new LC_242();
+        System.out.println(lc_242.isAnagram("rat","car"));
+    }
+}
---
+++ src/main/java/com/sindhuk/leetcode/ds1/LC_383.java
@@ -0,0 +1,29 @@
+package main.java.com.sindhuk.leetcode.ds1;
+
+public class LC_383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int count[] = new int[26];
+        for(char ch: ransomNote.toCharArray()){
+            count[Integer.valueOf(ch) - 97] += 1;
+        }
+
+        for(char ch: magazine.toCharArray()){
+            if(ransomNote.indexOf(ch) != -1) {
+                count[Integer.valueOf(ch) - 97] -= 1;
+            }
+        }
+
+        for(int i=0;i<26; i++){
+            if(count[i] > 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] arg){
+        LC_383 lc_383 = new LC_383();
+        System.out.println(lc_383.canConstruct("az","ab"));
+    }
+}
